43ea84278d51441c3f16a21503fea85ede7ce1d0
Philip Hughes <phugh80@hotmail.com>
2020-04-22 13:21:26 +0100

    Adding function to retrieve all appointments, filter them by doctorId, and render the results in the index.html file. Work in progress

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, redirect, render_template, request, session, url_for
 from flask_pymongo import PyMongo
+from datetime import datetime
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -11,8 +13,23 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('base.html')
+def index():
+    today = datetime.now().strftime("%d/%m/%Y")
+    doctor = "5e8f51b51c9d440000598471"
+    appointments = mongo.db.appointments.find({"date": today})
+    new_appointments = []
+    for appointment in appointments:
+        if len(appointment["appointment_list"]) != 0:
+           test = next((item for item in appointment["appointment_list"] if item["doctor_id"] == doctor), None)
+           if test != None:
+               patient = mongo.db.patients.find_one({"_id": ObjectId(test["patient_id"])})
+               new_appointments.append({"id": appointment["_id"], "time":appointment["time"], "patient": patient, "empty": False})
+           else:
+               new_appointments.append({"id": appointment["_id"], "time":appointment["time"], "empty": True})
+        else:
+            new_appointments.append({"id": appointment["_id"], "time":appointment["time"], "empty": True})
+
+    return render_template("index.html", slots = new_appointments )
 
 
 if __name__ == '__main__':

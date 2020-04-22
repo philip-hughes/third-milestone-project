527ad8af2e103e5d2ca6b03d5af234b9abe61265
Philip Hughes <phugh80@hotmail.com>
2020-04-22 14:28:17 +0100

    Updating the addAppointment function to find patients with the defined doctorID. Also passing the appointment time and id to the addAppointment template

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = "appointment_scheduler"
-app.config["MONGO_URI"] = "mongodb+srv://philiph80:delrod123@myfirstcluster-yuvii.mongodb.net/appointment_scheduler?retryWrites=true&w=majority"
+app.config[
+    "MONGO_URI"] = "mongodb+srv://philiph80:delrod123@myfirstcluster-yuvii.mongodb.net/appointment_scheduler?retryWrites=true&w=majority"
 
 mongo = PyMongo(app)
 
@@ -20,20 +21,25 @@ def index():
     new_appointments = []
     for appointment in appointments:
         if len(appointment["appointment_list"]) != 0:
-           test = next((item for item in appointment["appointment_list"] if item["doctor_id"] == doctor), None)
-           if test != None:
-               patient = mongo.db.patients.find_one({"_id": ObjectId(test["patient_id"])})
-               new_appointments.append({"id": appointment["_id"], "time":appointment["time"], "patient": patient, "empty": False})
-           else:
-               new_appointments.append({"id": appointment["_id"], "time":appointment["time"], "empty": True})
+            test = next((item for item in appointment["appointment_list"] if item["doctor_id"] == doctor), None)
+            if test != None:
+                patient = mongo.db.patients.find_one({"_id": ObjectId(test["patient_id"])})
+                new_appointments.append(
+                    {"id": appointment["_id"], "time": appointment["time"], "patient": patient, "empty": False})
+            else:
+                new_appointments.append({"id": appointment["_id"], "time": appointment["time"], "empty": True})
         else:
-            new_appointments.append({"id": appointment["_id"], "time":appointment["time"], "empty": True})
+            new_appointments.append({"id": appointment["_id"], "time": appointment["time"], "empty": True})
 
-    return render_template("index.html", slots = new_appointments)
+    return render_template("index.html", slots=new_appointments)
 
-@app.route('/addAppointment.html/<appointmentId>/<time>')
+
+@app.route('/addAppointment/<appointmentId>/<time>', methods=["POST", "GET"])
 def addAppointment(time, appointmentId):
-    return render_template('addAppointment.html')
+    doctor = "5e8f51b51c9d440000598471"
+    patients = mongo.db.patients.find({"doctor_id": doctor})
+    return render_template("addAppointment.html", time=time, appointmentId=appointmentId, patients=patients)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

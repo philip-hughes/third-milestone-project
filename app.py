cad98f7629cef344dd859362dd72c08d2c554b29
Philip Hughes <phugh80@hotmail.com>
2020-04-22 01:08:37 +0100

    Initial commit, including mongoDB connection setup and imports of required libraries

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+import os
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = "appointment_scheduler"
+app.config["MONGO_URI"] = "mongodb+srv://philiph80:delrod123@myfirstcluster-yuvii.mongodb.net/appointment_scheduler?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('base.html')
 
 
 if __name__ == '__main__':

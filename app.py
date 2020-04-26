c2335b0978eb14fa3759db6211cd85b3a7033610
Philip Hughes <phugh80@hotmail.com>
2020-04-26 23:25:54 +0100

    Adding getAppointments function

--- app.py
+++ app.py
@@ -17,21 +17,27 @@ mongo = PyMongo(app)
 def index():
     today = datetime.now().strftime("%d/%m/%Y")
     doctor = "5e8f51b51c9d440000598471"
-    appointments = mongo.db.appointments.find({"date": today})
-    new_appointments = []
-    for appointment in appointments:
-        if len(appointment["appointment_list"]) != 0:
-            test = next((item for item in appointment["appointment_list"] if item["doctor_id"] == doctor), None)
-            if test != None:
-                patient = mongo.db.patients.find_one({"_id": ObjectId(test["patient_id"])})
-                new_appointments.append(
-                    {"id": appointment["_id"], "time": appointment["time"], "patient": patient, "empty": False})
+    hours = mongo.db.appointments.find({"date": today})
+    filtered_hours = []
+    for hour in hours:
+        slots = []
+        for time_obj in hour["appointment_list"]:
+            print("timeobj ", time_obj)
+            if len(time_obj["appointments"]) != 0:
+                print("time_obj apps", time_obj["appointments"])
+                test = next((item for item in time_obj["appointments"] if item["doctor_id"] == doctor), None)
+                if test != None:
+                    patient = mongo.db.patients.find_one({"_id": ObjectId(test["patient_id"])})
+                    slots.append(
+                        {"time": time_obj["time"], "patient": patient, "empty": False})
+                else:
+                    slots.append({"time": time_obj["time"], "empty": True})
             else:
-                new_appointments.append({"id": appointment["_id"], "time": appointment["time"], "empty": True})
-        else:
-            new_appointments.append({"id": appointment["_id"], "time": appointment["time"], "empty": True})
+                slots.append({"time": time_obj["time"], "empty": True})
 
-    return render_template("index.html", slots=new_appointments)
+        filtered_hours.append({"id": hour["_id"], "hour": hour["hour"], "slots": slots})
+        print("hours", filtered_hours)
+    return render_template("index.html", hours=filtered_hours)
 
 
 @app.route('/addAppointment/<appointmentId>/<time>', methods=["POST", "GET"])
@@ -75,6 +81,25 @@ def insert_patient():
     patients = mongo.db.patients
     patients.insert_one(request.form.to_dict())
     return redirect(url_for('index'))
+
+
+def get_appointments(date, doctor_id):
+    filtered_appointments = []
+    slot = mongo.db.slots.find_one({"date": date})
+    print('slot ', slot)
+    appointment_ids = slot["appointment_ids"]
+    print("appointment_ids ", appointment_ids)
+    for appointment_id in appointment_ids:
+        appointment = mongo.db.appointments.find_one({
+            '$and': [{"_id": ObjectId(appointment_id),
+                      "doctor_id": doctor_id}]
+        })
+        print("appointment ", appointment)
+        if appointment is not None:
+            filtered_appointments.append(appointment)
+
+    return filtered_appointments
+
 
 if __name__ == '__main__':
     app.run(debug=True)

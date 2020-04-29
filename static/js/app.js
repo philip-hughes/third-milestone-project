bba6975b092e525d4b2b566d762231b06cb649db
Philip Hughes <phugh80@hotmail.com>
2020-04-29 02:45:09 +0100

    Adding function that retrieves the time from the selected slot and passes it to the modal form

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
-    $(".remove").click(function(){
+   /** $(".remove").click(function(){
         const id = $(this).attr("data-id");
         const url = "/remove_appointment/".concat(id);
         location.href=url;
         })
 
     $('#myTabs a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
+    e.preventDefault()
+    $(this).tab('show')**/
+
+      $('#newAppointmentModal').on('show.bs.modal', function (event) {
+      const slot = $(event.relatedTarget); // Slot that triggered the modal
+      const time = slot.data('time');
+      const modal = $(this);
+       modal.find('.modal-body input').val(time)
+    })
 });

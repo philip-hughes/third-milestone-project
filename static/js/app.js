78753856660e2c57310a2b96714c7e914ba9e43f
Philip Hughes <phugh80@hotmail.com>
2020-04-29 16:43:39 +0100

    Adding logic to determine how many consecutive slots are available after the select start time. This will be used to populate the end time dropdown in the modal

--- static/js/app.js
+++ static/js/app.js
@@ -9,10 +9,42 @@ $(document).ready(function() {
     e.preventDefault()
     $(this).tab('show')**/
 
-      $('#newAppointmentModal').on('show.bs.modal', function (event) {
-      const slot = $(event.relatedTarget); // Slot that triggered the modal
-      const time = slot.data('time');
-      const modal = $(this);
-       modal.find('.modal-body input').val(time)
+
+$('#newAppointmentModal').on('show.bs.modal', function (event) {
+    const slot = $(event.relatedTarget); // Slot that triggered the modal
+    const time = slot.data('time');
+    const modal = $(this);
+    modal.find('.modal-body input').val(time); // Add the selected slot time to the modal input
+
+    const otherSlots = $(".slot").map(function (){
+        const slotTime = $(this).data("time")
+        console.log("other slot time:" + slotTime)
+            const base = new Date("1980-01-01 " + time);
+            const test = new Date("1980-01-01 " + slotTime);
+            if ((test > base)) {
+                return this }
     })
-});
+
+    const availableTimes = [];
+    for(i=0; i < otherSlots.length; i++){
+        const slotTime = $(otherSlots[i]).data("time")
+        const appointmentId = $(otherSlots[i]).data("id")
+        if(!appointmentId){
+            availableTimes.push(slotTime)}
+        else { break;}
+          }
+})
+     });
+     /****test***
+    var base = new Date("1980-01-01 09:30");
+    var test = new Date("1980-01-01 08:30:01");
+
+    if (test >= base){
+    console.log("test time is newer or equal to base time");
+
+}   else {
+    console.log ("test time is older than 9.30");
+
+}*/
+
+

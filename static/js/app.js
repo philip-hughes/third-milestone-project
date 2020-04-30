311ed5fa832d7e4216d43b4bd9ab0303e715b791
Philip Hughes <phugh80@hotmail.com>
2020-04-30 17:13:34 +0100

    Adding editAppointmentModal data population logic. Its currently the same as the newAppointment modal code, but with the addition of a line to populate the Cancel links href. Work in progress

--- static/js/app.js
+++ static/js/app.js
@@ -37,11 +37,41 @@ $('#newAppointmentModal').on('show.bs.modal', function (event) {
         else { break;}
           }
 
-console.log();
+})
 
-console.log(moment.utc('14:45','HH:mm').add(15,'minutes').format('HH:mm'));
+$('#editAppointmentModal').on('show.bs.modal', function (event) {
+    const slot = $(event.relatedTarget); // Slot that triggered the modal
+    const time = slot.data('time');
+    const modal = $(this);
+    const id = slot.data('id')
+    modal.find('.modal-body input').val(time); // Add the selected slot time to the modal input
+    modal.find(".modal-body a").attr("href", `/remove_appointment/${id}`)
+
+    const otherSlots = $(".slot").map(function (){
+        const slotTime = $(this).data("time")
+        console.log("other slot time:" + slotTime)
+            const base = new Date("1980-01-01 " + time);
+            const test = new Date("1980-01-01 " + slotTime);
+            if ((test >= base)) {
+                return this }
+    })
+
+
+    const endTimeList = $("#endTime");
+    endTimeList.empty()
+    for(i=0; i < otherSlots.length; i++){
+        const slotTime = $(otherSlots[i]).data("time")
+        const appointmentId = $(otherSlots[i]).data("id")
+        if(!appointmentId){
+            const displayTime = moment.utc(slotTime,'HH:mm').add(15,'minutes').format('HH:mm')
+            endTimeList.append(`<option value=${slotTime}>${displayTime}</option>`)}
+        else { break;}
+          }
+
 
 })
+
+
 });
      /****test***
     var base = new Date("1980-01-01 09:30");

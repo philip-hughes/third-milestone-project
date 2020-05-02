1f8f17af38b604a368e07109148fb5ab1b7c081a
Philip Hughes <phugh80@hotmail.com>
2020-05-02 17:04:49 +0100

    Updating the get_times function to take the time data from a local json file

--- app.py
+++ app.py
@@ -122,9 +122,8 @@ def insert_appointment():
 
 
 def get_times(start_time, end_time):
-    times = ["09:00", "09:15", "09:30", "09:45", "10:00", "10:15", "10:30", "10:45",
-             "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45",
-             "13:00", "13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45"]
+    with open('times.json') as json_times:
+        times = json.load(json_times)
 
     filtered_times = times[times.index(start_time):times.index(end_time) + 1]
     times_list = []

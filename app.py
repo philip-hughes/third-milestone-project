452089d7f600b6fbc2ca5317d35e2251d52dcc97
Philip Hughes <phugh80@hotmail.com>
2020-05-02 15:39:48 +0100

    Moving functions into more logical order for easier reading

--- app.py
+++ app.py
@@ -20,23 +20,82 @@ def entry_page():
     return render_template('entry_page.html', doctors=doctors)
 
 
+@app.route('/set_doctor', methods=['POST', 'GET'])
+def set_doctor():
+    global selected_doctor_id
+    selected_doctor_id = request.form.get('doctor_id')
+    return redirect("/calendar")
+
+
 @app.route('/calendar', methods=['POST', 'GET'])
 @app.route('/calendar/', methods=['POST', 'GET'])
 def calendar():
     today = datetime.now().strftime("%d/%m/%Y")
     doctors = mongo.db.doctors.find()
     doctor = selected_doctor_id
-    calendar = build_calendar(today, doctor)
+    calendar = build_calendar(today)
     patients = list(mongo.db.patients.find())
     slot_id = mongo.db.slots.find_one({"date": today})["_id"]
     return render_template("calendar.html", calendar=calendar, patients=patients, slot_id=slot_id, doctors=doctors, doctor=doctor)
 
 
-@app.route('/set_doctor', methods=['POST', 'GET'])
-def set_doctor():
-    global selected_doctor_id
-    selected_doctor_id = request.form.get('doctor_id')
-    return redirect("/calendar")
+def build_calendar(date):
+    calendar = []
+    hours = [{"hour": "09:00", "times": ["09:00", "09:15", "09:30", "09:45"]},
+             {"hour": "10:00", "times": ["10:00", "10:15", "10:30", "10:45"]},
+             {"hour": "11:00", "times": ["11:00", "11:15", "11:30", "11:45"]},
+             {"hour": "12:00", "times": ["12:00", "12:15", "12:30", "12:45"]},
+             {"hour": "13:00", "times": ["13:00", "13:15", "13:30", "13:45"]},
+             {"hour": "14:00", "times": ["14:00", "14:15", "14:30", "14:45"]}]
+
+    appointments = get_appointments(date)
+    for hour in hours:
+        appointment_times = []
+        for i in range(4):
+            time = hour["times"][i]
+            appointment = search_appointments(appointments, time)
+            if appointment is not None:
+                time_obj = next((item for item in appointment["times"] if item["time"] == time), None)
+                appointment_times.append({"time": time,
+                                          "empty": False,
+                                          "first_time": time_obj["start_time"],
+                                          "last_time": time_obj["end"
+                                                                "_time"],
+                                          "appointment_id": appointment["_id"],
+                                          "patient_id": appointment["patient_id"],
+                                          "patient_name": appointment["patient_details"]["name"]})
+            else:
+                appointment_times.append({"time": time, "empty": True})
+
+        calendar.append({"hour": hour["hour"], "times": appointment_times})
+
+    return calendar
+
+
+def get_appointments(date):
+    filtered_appointments = []
+    slot = mongo.db.slots.find_one({"date": date})
+    appointment_ids = slot["appointment_ids"]
+    for appointment_id in appointment_ids:
+        appointment = mongo.db.appointments.find_one({
+            '$and': [{"_id": ObjectId(appointment_id),
+                      "doctor_id": selected_doctor_id}]
+        })
+        if appointment is not None:
+            patient = mongo.db.patients.find_one({"_id": ObjectId(appointment['patient_id'])})
+            appointment.update({'patient_details': patient})
+            filtered_appointments.append(appointment)
+    return filtered_appointments
+
+
+def search_appointments(appointments, time):
+    for appointment in appointments:
+        times = appointment["times"]
+        test = next((item for item in times if item["time"] == time), None)
+        if test is not None:
+            return appointment
+
+    return None
 
 
 @app.route('/insert_appointment', methods=["POST"])
@@ -114,65 +173,6 @@ def insert_patient():
     patients = mongo.db.patients
     patients.insert_one(request.form.to_dict())
     return redirect(url_for('index'))
-
-
-def build_calendar(date):
-    calendar = []
-    hours = [{"hour": "09:00", "times": ["09:00", "09:15", "09:30", "09:45"]},
-             {"hour": "10:00", "times": ["10:00", "10:15", "10:30", "10:45"]},
-             {"hour": "11:00", "times": ["11:00", "11:15", "11:30", "11:45"]},
-             {"hour": "12:00", "times": ["12:00", "12:15", "12:30", "12:45"]},
-             {"hour": "13:00", "times": ["13:00", "13:15", "13:30", "13:45"]},
-             {"hour": "14:00", "times": ["14:00", "14:15", "14:30", "14:45"]}]
-
-    appointments = get_appointments(date, selected_doctor_id)
-    for hour in hours:
-        appointment_times = []
-        for i in range(4):
-            time = hour["times"][i]
-            appointment = search_appointments(appointments, time)
-            if appointment is not None:
-                time_obj = next((item for item in appointment["times"] if item["time"] == time), None)
-                appointment_times.append({"time": time,
-                                          "empty": False,
-                                          "first_time": time_obj["start_time"],
-                                          "last_time": time_obj["end"
-                                                                "_time"],
-                                          "appointment_id": appointment["_id"],
-                                          "patient_id": appointment["patient_id"],
-                                          "patient_name": appointment["patient_details"]["name"]})
-            else:
-                appointment_times.append({"time": time, "empty": True})
-
-        calendar.append({"hour": hour["hour"], "times": appointment_times})
-
-    return calendar
-
-
-def search_appointments(appointments, time):
-    for appointment in appointments:
-        times = appointment["times"]
-        test = next((item for item in times if item["time"] == time), None)
-        if test is not None:
-            return appointment
-
-    return None
-
-
-def get_appointments(date):
-    filtered_appointments = []
-    slot = mongo.db.slots.find_one({"date": date})
-    appointment_ids = slot["appointment_ids"]
-    for appointment_id in appointment_ids:
-        appointment = mongo.db.appointments.find_one({
-            '$and': [{"_id": ObjectId(appointment_id),
-                      "doctor_id": selected_doctor_id}]
-        })
-        if appointment is not None:
-            patient = mongo.db.patients.find_one({"_id": ObjectId(appointment['patient_id'])})
-            appointment.update({'patient_details': patient})
-            filtered_appointments.append(appointment)
-    return filtered_appointments
 
 
 if __name__ == '__main__':

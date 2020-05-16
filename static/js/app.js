b197ffb4ce6f886fd8f4a34db3f73185a8f10949
Philip Hughes <phugh80@hotmail.com>
2020-05-16 15:31:40 +0100

    Update to close the sidebar and hide the overlay when the overlay is clicked

--- static/js/app.js
+++ static/js/app.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
     }
    });
 
-    $('.sidebarCollapse').on('click', function () {
+    $('.sidebarCollapse, .overlay').on('click', function () {
         $('#sidebar').toggleClass('active');
         $('.overlay').toggleClass('active');
     });
